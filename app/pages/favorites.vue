71528b0c2d77764bf7ddc06c3f0b504a8cb093fb
AlperAyik <alperayik06@gmail.com>
2026-07-20 02:25:52 +0200

    changes favorites page

--- app/pages/favorites.vue
+++ app/pages/favorites.vue
@@ -16,10 +16,11 @@ onMounted(() => {
   <navbar></navbar>
 <!--laat alle saved dates zien-->
 
-  <div class="grid grid-cols-3 lg:grid-cols-4" v-if="favorites.length > 0">
-    <div v-for="favo in favorites" class="">
-      <h1>{{favo.titel}}</h1>
-
+  <div class="m-20" v-if="favorites.length > 0">
+    <div class="grid grid-cols-4 lg:grid-cols-4 gap-10">
+      <div v-for="favo in favorites" class="bg-amber-200 w-fit p-10 rounded-md">
+        <NuxtLink :to="`/datePages/${favo.slug}`"><h1>{{favo.titel}}</h1></NuxtLink>
+      </div>
     </div>
   </div>
 
